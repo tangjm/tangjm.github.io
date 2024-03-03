006bd4d5f0594daa506d60612dba63be665c5d9d
tangjm <tangjm.dev@outlook.com>
2024-03-03 11:00:51 +0000

    Update page title

--- components/layout.js
+++ components/layout.js
@@ -4,8 +4,7 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
-const name = "Shu Uesugi";
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle = "tangjm's learning log";
 
 export default function Layout({ children, home }) {
   return (
